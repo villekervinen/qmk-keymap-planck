e0b2cf66ed343299a582ea9624f39a44b7962c2b
Ville Kervinen <ville.kervinen@gmail.com>
2019-03-16 19:03:38 +0200

    Add gaming layer

--- keymap.c
+++ keymap.c
@@ -7,8 +7,12 @@ enum planck_layers {
   _DEFAULT,
   _LOWER,
   _RAISE,
-  _ADJUST
+  _ADJUST,
+  _GAMING
 };
+
+#define DEFAULT DF(_DEFAULT)
+#define GAMING DF(_GAMING)
 
 #define LOWER MO(_LOWER)
 #define RAISE MO(_RAISE)
@@ -23,14 +27,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
  * | Shift |   Z   |   X   |   C   |   V   |   B   |   N   |   M   |   ,   |   .   |   -   | Shift |
  * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
- * |       |  Ctrl |  Alt  |  GUI  | Lower | Enter | Space | Raise |       |       |       |       |
+ * |       |  Ctrl |  Alt  |  GUI  | Lower | Enter | Space | Raise |       |       |       | Gaming|
  * `-----------------------------------------------------------------------------------------------'
  */
 [_DEFAULT] = LAYOUT_planck_grid(
     KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,KC_BSPC,
     KC_ESC, KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,NO_OSLH,NO_AE  ,
     KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_B   ,KC_N   ,KC_M   ,KC_COMM,KC_DOT ,NO_MINS,KC_RSFT,
-    _______,KC_LCTL,KC_LALT,KC_LGUI,LOWER  ,KC_ENT ,KC_SPC ,RAISE  ,_______,_______,_______,_______
+    _______,KC_LCTL,KC_LALT,KC_LGUI,LOWER  ,KC_ENT ,KC_SPC ,RAISE  ,_______,_______,_______,GAMING
 ),
 
 /* Lower
@@ -85,8 +89,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______,_______,_______,_______,_______,_______,_______,_______,KC_MPLY,KC_VOLU,KC_MUTE,_______,
     _______,_______,_______,_______,_______,_______,_______,_______,KC_MPRV,KC_VOLD,KC_MNXT,_______,
     _______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______,_______
-)
+),
 
+/* Gaming
+ * ,-----------------------------------------------------------------------------------------------.
+ * |  Esc  |   1   |   2   |   3   |   4   |   5   |   6   |   7   |   8   |   9   |   0   |  Bksp |
+ * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
+ * |  Tab  |   Q   |   W   |   E   |   R   |   T   |   Y   |   U   |   I   |   O   |   P   |   -   |
+ * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
+ * |  Ctrl |   A   |   S   |   D   |   F   |   G   |   H   |   J   |   K   |   L   |   ,   |   .   |
+ * |-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------+-------|
+ * | Shift |   Z   |   X   |   C   |   V   | Space |   B   |   N   |   M   |   Alt |  GUI  |Default|
+ * `-----------------------------------------------------------------------------------------------'
+ */
+[_GAMING] = LAYOUT_planck_grid(
+    KC_ESC ,KC_1   ,KC_2   ,KC_3   ,KC_4   ,KC_5   ,KC_6   ,KC_7   ,KC_8   ,KC_9   ,KC_0   ,KC_BSPC,
+    KC_TAB ,KC_Q   ,KC_W   ,KC_E   ,KC_R   ,KC_T   ,KC_Y   ,KC_U   ,KC_I   ,KC_O   ,KC_P   ,NO_MINS,
+    KC_LCTL,KC_A   ,KC_S   ,KC_D   ,KC_F   ,KC_G   ,KC_H   ,KC_J   ,KC_K   ,KC_L   ,KC_COMM,KC_DOT ,
+    KC_LSFT,KC_Z   ,KC_X   ,KC_C   ,KC_V   ,KC_SPC ,KC_B   ,KC_N   ,KC_M   ,KC_RALT,KC_RGUI,DEFAULT
+),
 
 };
 
